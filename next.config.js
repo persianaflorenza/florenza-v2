54b44c0466893abbe13ae3c9789f510177f645c0
Luan <luanfelipecosta@gmail.com>
2021-12-13 20:39:48 -0300

    setup graphql / codegen

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ['www.datocms-assets.com'],
+  },
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.(graphql|gql)$/,
+      exclude: /node_modules/,
+      loader: 'graphql-tag/loader',
+    });
+    return config;
+  },
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,24 @@
+import { HomepageContent } from '@src/generated/schema';
 import type { NextPage } from 'next';
+import HomePageQuery from '@src/queries/homepage.gql';
+import { contentService } from '@src/services';
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ data }: any) => {
+  console.log(data)
   return (
     <div>
-      <h1 className="text-navy-500 text-7xl">Hello world</h1>
+      <h1 className="text-navy-500 text-7xl">Hello worlder</h1>
     </div>
   );
 };
 
-export const getStaticProps = () => {
+export const getStaticProps = async () => {
+  const data = await contentService.query<HomepageContent, {}>({
+    query: HomePageQuery,
+  });
+
   return {
-    props: { count: 10 },
+    props: { data },
   };
 };
 
